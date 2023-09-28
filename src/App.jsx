a1fd3599bcc0c22e4d688a8dcae50d31f4e30f4c
AlexisNtec <ferney.naranjo@novatec.com.co>
2023-09-28 10:37:14 -0500

    fix: visualizacion de fecha

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
-import { Heading, Box, Image, Flex, Text, Spacer, Tag } from "@chakra-ui/react";
-import { FiCalendar } from "react-icons/fi"
+import { Center, Heading, Image} from "@chakra-ui/react";
+import { LaunchItem } from "./components/LaunchItem";
 import * as API from "./services/launches"
 import logo from "./assets/log-spacex.png"
 
@@ -19,29 +19,7 @@ function App() {
       </Heading>
       <section>
         {launches.map(launch=> (
-          <Box 
-            key={launch.fligth_number} 
-            bg="gray.100" 
-            p={4} m="4" 
-            borderRadius="lg"
-          >
-            <Flex display="flex">
-              <Text fontSize="2Xl">
-                Mission <strong>{launch.mission_name}</strong>({launch.launch_year})
-              </Text>
-              <Spacer/>
-              <Tag p={4} colorScheme= {launch.launch_success ? "green" : "red"}>
-                {launch.launch_success ? "Succes" : "Failure"}
-              </Tag>
-            </Flex>
-
-            <Flex align="center">
-              <FiCalendar />
-              <Text fontSize="sm" ml={1}>
-                {launch.launch_date_local.split("T")[0]}
-              </Text>
-            </Flex>
-          </Box>
+          <LaunchItem key={launch.fligth_number} {...launch}/>
         ))}
       </section>
     </>
